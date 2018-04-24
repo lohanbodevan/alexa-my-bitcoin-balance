9db41e2f3d9a34c2613a26e97a032ddefa75ef7b
Lohan Bodevan <lohan.bodevan@gmail.com>
2018-04-23 21:09:12 -0300

    Add catch for promises and preserve this for alexa
    Preserve this for Alexa handlers, even using arrow function

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ const Alexa = require('alexa-sdk');
 const { BlinkTradeRest } = require('blinktrade');
 
 const myBitcoinBalance = blinktrade =>
-  blinktrade.balance().then(data => {
-    const satoshiValue = data['4'].BTC;
-    const bitcoin = satoshiValue * 0.00000001;
+  blinktrade
+    .balance()
+    .then(data => {
+      const satoshiValue = data['4'].BTC;
+      const bitcoin = satoshiValue * 0.00000001;
 
-    return blinktrade.ticker().then(ticker => {
-      const balance = bitcoin * ticker.sell;
-      return `Your balance is ${balance.toFixed(2)} ${process.env.CURRENCY}`;
+      return blinktrade
+        .ticker()
+        .then(ticker => {
+          const balance = bitcoin * ticker.sell;
+          return `Your balance is ${balance.toFixed(2)} ${
+            process.env.CURRENCY
+          }`;
+        })
+        .catch(err => {
+          console.error('Fail to get ticker', err);
+        });
+    })
+    .catch(err => {
+      console.error('Fail to get balance', err);
     });
-  });
 
 const alexaHandlers = {
-  LaunchRequest: () => {
+  LaunchRequest() {
     const blinktrade = new BlinkTradeRest({
       prod: true,
       key: process.env.BLINK_TRADE_KEY,
@@ -21,13 +33,19 @@ const alexaHandlers = {
       currency: process.env.CURRENCY_CODE,
     });
 
-    myBitcoinBalance(blinktrade).then(balance => {
-      const cardTitle = 'My Bitcoin Balance';
-      const cardContent = balance;
+    const self = this;
 
-      this.response.speak(balance).cardRenderer(cardTitle, cardContent);
-      this.emit(':responseReady');
-    });
+    myBitcoinBalance(blinktrade)
+      .then(balance => {
+        const cardTitle = 'My Bitcoin Balance';
+        const cardContent = balance;
+
+        self.response.speak(balance).cardRenderer(cardTitle, cardContent);
+        self.emit(':responseReady');
+      })
+      .catch(err => {
+        console.error('Fail to handle with response', err);
+      });
   },
 };
 
